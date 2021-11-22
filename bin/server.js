bd7b91b61e8158b49c14b517d0501f3220dd1302
sepaev <sepaev@gmail.com>
2021-11-23 00:08:37 +0200

    мангуст + тестовый getAll

--- bin/server.js
+++ bin/server.js
@@ -1,8 +1,20 @@
 const app = require('../app')
+require('dotenv').config()
+const mongoose = require('mongoose')
 
-const PORT = process.env.PORT || 4321
+const { DB_HOST, PORT = 3000 } = process.env
 
-app.listen(PORT, err => {
-  if (err) console.error('Error at server Launch:', err)
-  console.log(`Server running. Use our API on port: ${PORT}`)
-})
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    console.log('Database connection successful')
+
+    app.listen(PORT, err => {
+      if (err) console.error('Error at server Launch:', err)
+      console.log(`Server running. Use our API on port: ${PORT}`)
+    })
+  })
+  .catch(error => {
+    console.log(error.message)
+    process.exit(1)
+  })
--- model/listContacts.js
+++ model/listContacts.js
@@ -1,13 +1,8 @@
-const path = require('path')
-const fs = require('fs').promises
-const { dbPath } = require('../bin/settings')
-console.log(' dbPath - ', dbPath)
-const contactsPath = path.resolve(dbPath)
+const Contact = require('../model/contact')
 
 async function listContacts() {
   try {
-    const data = await fs.readFile(contactsPath, 'utf8')
-    const contacts = JSON.parse(data)
+    const contacts = await Contact.find({})
     return contacts
   } catch (error) {
     console.log('Catch error', error.message)
